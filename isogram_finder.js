649ed2be2edb6c619fc5f94224326a3fd7d2b1fa
Grilo16 <98457056+Grilo16@users.noreply.github.com>
2022-08-31 22:55:23 +0100

    finished isogram

--- isogram_finder.js
+++ isogram_finder.js
@@ -1,9 +1,16 @@
 const IsogramFinder = function (word) {
-
+    this.word = word.toLowerCase().split("")
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    return this.word.every((char, index) => {
+        return this.word.indexOf(char) === index
+    });
 }
 
 module.exports = IsogramFinder;
+
+const isogramFinder = new IsogramFinder('Uncopyrightable');
+const isogramFinder1 = new IsogramFinder('NotAnIsogram');
+isogramFinder.isIsogram()
+isogramFinder1.isIsogram()
